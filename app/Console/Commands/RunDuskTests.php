3b84592d1da86a61fc377a7cb0855d1dc243504c
Armen Meltonyan <armmeltonyan@gmail.com>
2023-12-30 19:58:25 +0100

    AddLogs

--- app/Console/Commands/RunDuskTests.php
+++ app/Console/Commands/RunDuskTests.php
@@ -8,6 +8,7 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 
 class RunDuskTests extends Command
 {
@@ -31,8 +32,13 @@ class RunDuskTests extends Command
     public function handle(TopMsService $topMsService)
     {
         $iterations = Iteration::where('processed',0)->get();
+        Log::error($iterations);
         foreach ($iterations as $iteration) {
+            Log::error('db date:'. Carbon::parse($iteration->process_at)->toDateString());
+            Log::error('now date:'. Carbon::now()->toDateString());
             if (Carbon::parse($iteration->process_at)->toDateString() == Carbon::now()->toDateString()){
+                Log::error('now time:'. Carbon::now());
+                Log::error('db time:'. Carbon::parse($iteration->time));
                 if (Carbon::now()->greaterThan(Carbon::parse($iteration->time))){
                     echo 'processing';
                     echo $iteration->server_name;
