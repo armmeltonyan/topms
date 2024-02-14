f52fcf20e9bea6dd7febe94789861a884b2b76d2
Armen Meltonyan <armmeltonyan@gmail.com>
2024-02-14 12:24:06 +0400

    V2

--- app/Console/Commands/RunDuskTests.php
+++ app/Console/Commands/RunDuskTests.php
@@ -44,6 +44,7 @@ class RunDuskTests extends Command
                     echo $iteration->server_name;
                     for ($x = 1; $x <= $iteration->iteration; $x++) {
                         $response = $topMsService->buy($iteration->server_name,1,$iteration->monitoring);
+                        Log::error("response: ".$response);
                         sleep(1);
                     }
 
